5838c5fd90429b343208258330f155bdbf4fec80
rijkvp <github@rijkvp.nl>
2022-01-28 17:02:00 +0100

    Add detailed RSS route information to feeds

--- src/feed.rs
+++ src/feed.rs
@@ -1,17 +1,34 @@
 use crate::{config::FeedConfig, post::Post};
 use serde::Serialize;
 use std::path::PathBuf;
+
 #[derive(Debug, Serialize)]
 pub struct Feed {
-    pub rss_path: Option<PathBuf>,
     pub posts: Vec<Post>,
+    pub rss: Option<RssInfo>,
+}
+
+#[derive(Debug, Serialize)]
+pub struct RssInfo {
+    pub path: PathBuf,
+    pub route: PathBuf,
 }
 
 impl Feed {
     pub fn new(posts: Vec<Post>, config: &FeedConfig) -> Self {
+        let rss = {
+            if let Some(rss_path) = &config.rss_path {
+                Some(RssInfo {
+                    path: rss_path.clone(),
+                    route: PathBuf::from("/").join(&rss_path),
+                })
+            } else {
+                None
+            }
+        };
         Self {
-            rss_path: config.rss_path.clone(),
             posts,
+            rss,
         }
     }
 }
